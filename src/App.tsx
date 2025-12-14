a8249ec5f8fe64780a3e5ba02c2fa59f3998635f
Olli Salli <ollisal@users.noreply.github.com>
2025-12-14 20:18:12 +0200

    Remove button click counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import * as stylex from '@stylexjs/stylex'
 
-import { Button } from './components/Button'
-import { Card } from './components/Card'
 import { MainHeading } from './components/Heading'
 import { ViteLogo, ReactLogo } from './components/Logo'
-
-import { useCounter } from './store/counter'
 
 const styles = stylex.create({
   readTheDocs: {
@@ -14,8 +10,6 @@ const styles = stylex.create({
 })
 
 function App() {
-  const { count, increment: incrementCount } = useCounter()
-
   return (
     <>
       <div>
@@ -24,13 +18,6 @@ function App() {
       </div>
 
       <MainHeading>Vite + React + Zustand</MainHeading>
-
-      <Card>
-        <Button onClick={incrementCount}>count is {count}</Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </Card>
 
       <p {...stylex.props(styles.readTheDocs)}>Click on the Vite and React logos to learn more</p>
     </>
